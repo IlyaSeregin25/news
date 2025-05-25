f085cb12538400c5f65d493f39c061f31ffa0301
Ilya99 <Ilya99@mail.ru>
2025-05-25 18:31:56 +0400

    change apiNews

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { useState } from 'react';
 import Header from './components/Header/Header';
 import Main from './pages/Main/Main';
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
   return (
-    <>
+    <div className={`app ${isDark ? 'dark' : 'light'}`}>
       <Header />
       <div className="container">
         <Main />
       </div>
-    </>
+    </div>
   );
 }
 
